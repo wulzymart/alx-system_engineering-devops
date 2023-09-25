56c27a317bf3ba9802b4854fe605973300236f9c
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-25 23:34:50 +0100

    task 100

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+# install and configure an Nginx server using Puppet
+exec {'update apt-get and install':
+    command  => 'sudo apt-get update && sudo apt-get install nginx',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
+}
+
+exec {'add landing page':
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
+}
+
+exec {'redirection':
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me \/ permanent;/" /etc/nginx/sites-available/default',
+  provider => shell,
+}
+
+exec { 'allow firewall':
+    command => 'ufw allow "NGINX Full"',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+}
+
+exec { 'restart nginx':
+    command => 'sudo service nginx restart',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+
+}
