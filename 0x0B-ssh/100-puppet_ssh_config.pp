e6ad15695b9e85c73c5e6adda4495001357a13bb
Olawole Olawale Martins <olawalemart@gmail.com>
2023-09-25 01:00:13 +0100

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+file { '/etc/ssh/ssh_config':
+    ensure  => 'present',
+}->
+
+file_line { 'identity file':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => '     IdentityFile ~/.ssh/school',
+}
+
+file_line { 'no password':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => '     PasswordAuthentication no',
+}
