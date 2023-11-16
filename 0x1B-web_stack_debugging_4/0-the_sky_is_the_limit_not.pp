c748c3b3f2c606cc78891d4ccd5d84bb5f17a234
Olawole Olawale Martins <olawalemart@gmail.com>
2023-11-16 19:19:28 +0100

    mandatory tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Free nginx 
+
+# Increase the limit
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/7000/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# eestart nginx
+-> exec { 'nginx-restart':
+  command => 'service nginx restart',
+  path    => '/usr/local/bin/:/bin/'
+}
